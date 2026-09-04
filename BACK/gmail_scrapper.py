f356e915357d54c91736ca05e6d0779ed1b9c8d5
HL-Neto <humbertolunaneto@gmail.com>
2026-09-04 13:28:46 -0300

    VER 2.5  Add. Adicionado 90% do sistema de rag , falta apenas fazer a concilição com a ia

--- BACK/gmail_scrapper.py
+++ BACK/gmail_scrapper.py
@@ -1,8 +1,20 @@
+import os
 import base64
-from authentication import conectar_gmail
+
+from Auth.authentication import conectar_gmail
 from pypdf import PdfReader
 
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
 
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 #como vai se tornar um sistema que vai continuar na empresa depois que eu sair, vou tentar deixar tudo mais documentado possivel
 
 
@@ -270,15 +282,91 @@ def get_curriculum():
 
         curriculo.append({
 
-            "id :"        : anexo["id"],
-            "remetente :" : anexo["remetente"],
-            "curriculo :" : full_text
+            "id"        : anexo["id"],
+            "remetente" : anexo["remetente"],
+            "curriculo" : full_text
         })
 
-    print(curriculo)
+    
     return curriculo
 
-get_curriculum()
 
+def splitter():
+
+    # aqui a gente vai dividir o curriculo em chunks para fazer o embendding e dps armazenar no bv
+    curriculos = get_curriculum()
+
+  
+    text_splitter = RecursiveCharacterTextSplitter(
+
+        chunk_size    = 500,
+        chunk_overlap = 50
+    )
+
+    split = []
+
+    #pegando o texto ja separado do get_curriculum() e corta ele, alem disso ele manda o id como metadata
+    for curriculo in curriculos:
+        
+        chunks = text_splitter.create_documents(
+            [curriculo["curriculo"]],
+        
+        metadatas = [{
+
+                "id": curriculo["id"],
+                "remetente": curriculo["remetente"]
+
+            }]
+        )
+
+        split.extend(chunks)
+
+    
+    return split 
+
+def embendding():
+
+    #iniciando o modelo de embendding 
+
+    embendding = HuggingFaceEmbeddings(
+
+        model_name    = "sentence-transformers/all-mpnet-base-v2",
+        encode_kwargs = {"normalize_embeddings": True}
+    )
+
+    return embendding
+
+
+
+
+def pinecone():
+
+
+    #instancias basicas do pinecone
+    pcapikey   = os.getenv("PINECONE_API_KEY")
+    pinecone   = Pinecone(api_key = pcapikey)
+    index_name = "RH_CURRICULO_ANALISADOR"
+
+    split      = splitter()
+    embendding = embendding()
+    
+    #verifica se existe um banco vetorial com esse nome , se n tiver ele cria
+    if not pinecone.has_index(index_name):
+
+        pinecone.create_index(
+
+            name      = index_name,
+            dimension = 768,
+            metric    = "cosine",
+            spec      = ServerlessSpec( cloud = "aws", region = "us-east-1")
+        )
+
+    index = pinecone.Index(index_name)
+
+
+
+
+    vector_store = PineconeVectorStore( index = index, embedding = embendding)
+    vector_store.add_documents(split)
 
 
